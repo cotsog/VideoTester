d5b00ff6a52cb05dc6158f906c5e5effc038aeb1
shehata_gamal <shehata.gamal@poseidon-me.com>
2019-04-17 19:03:21 +0200

    xzxzxzxzxzxz

--- create-keychain.sh
+++ create-keychain.sh
@@ -8,5 +8,7 @@ security default-keychain -s ios-build.keychain
 # Unlock the keychain
 security unlock-keychain -p travis ios-build.keychain
 # Add certificates to keychain and allow codesign to access them
-security import ./apple.cer -k ~/Library/Keychains/ios-build.keychain -A security import ./cert.cer -k ~/Library/Keychains/ios-build.keychain -A security import ./key.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
+security import ./apple.cer -k ~/Library/Keychains/ios-build.keychain -A
+security import ./cert.cer -k ~/Library/Keychains/ios-build.keychain -A
+security import ./key.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -A
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k travis ~/Library/Keychains/ios-build.keychain
