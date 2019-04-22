234308a4377483e5af2ccd1f2d182498d2f34b25
shehata_gamal <shehata.gamal@poseidon-me.com>
2019-04-22 18:51:04 +0200

    cxcxccxccxx

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 security list-keychains -s ios-build.keychain
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles/
-cp profile.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+cp "profile.mobileprovision" ~/Library/MobileDevice/Provisioning\ Profiles/
 xcrun xcodebuild -workspace VideoTester.xcworkspace -scheme VideoTester  -archivePath VideoTester.xcarchive archive
 xcrun xcodebuild -exportArchive -archivePath VideoTester.xcarchive  -exportPath . -exportOptionsPlist ExportOptions.plist
